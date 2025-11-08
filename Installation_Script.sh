d0936ac000fa7d1218804c7832f9cf7cc7cd88df
Murali-Kaspa <murali.kaspa26@gmail.com>
2025-11-08 14:12:40 +0530

    Jenkins_Script

---
+++ Installation_Script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo yum install java-17-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable Jenkins
+sudo usermod -aG jenkins $USER
+
+password=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+echo "Jenkins initial admin password is: ${password}"
