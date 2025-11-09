4b7f9309251088e81c7119a659a0b3a7b5b0ecfd
Murali-Kaspa <murali.kaspa26@gmail.com>
2025-11-09 21:09:25 +0530

    Update Installation_Script.sh

--- Installation_Script.sh
+++ Installation_Script.sh
@@ -6,7 +6,7 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 sudo yum install jenkins -y
 sudo systemctl start jenkins
-sudo systemctl enable Jenkins
+sudo systemctl enable jenkins
 sudo usermod -aG jenkins $USER
 echo "Jenkins Installed Successfully"
 password=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
